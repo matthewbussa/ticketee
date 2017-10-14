914aea8df4304bdd1d5190efcaadaa36c8bdcb3a
Matthew Bussa <matthewbussa@gmail.com>
2017-10-14 14:43:04 -0400

    added minimum length to ticket description and removed generated tests

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   validates :name, :description, presence: true
-  validates :description, length: {maximum: 1000}
+  validates :description, presence: true, length: {minimum: 10, maximum: 1000}
 end
